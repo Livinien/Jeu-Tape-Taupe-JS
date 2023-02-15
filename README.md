f2424181ddcc45409f3a2897241c162bfd4477f7
Flavien <livinien@hotmail.fr>
2023-02-15 16:13:40 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Tap Taupe
+
+1. Faire apparaitre aléatoirement mes taupes. (math.random) X
+2. Créer un interval entre mes différentes taupes. (setInterval) X
+3. Quand l'utilisateur clique sur la taupe alors il gagne un point.
+4. Lancer le jeu quand je clique sur mon bouton "PLAY".
---
+++ app.js
@@ -0,0 +1,35 @@
+
+
+$(document).ready(function() {
+
+  // Faire apparaitre et cacher les taupes de manière aleatoirement
+  
+  // $(".taupe").hide();
+
+  setInterval(function() {
+    let randomnumber = 1 + Math.floor(Math.random() * 3);
+    // console.log(randomnumber);
+
+    $('.n' + randomnumber).show(1).delay(1000).hide(1);
+    // console.log($(".n") + randomnumber);
+  }, 1500);
+  
+});
+
+
+// Ajouter le nombre de point au moment du clique sur la taupe
+
+let count = 0;
+
+$(document).ready(function() {
+  $('.taupe').click(function() {
+    count++;
+    $('.like').remove();
+    $('h1').append('<span class="like">'+ count +'</span>')
+  });
+});
+
+
+
+
+
